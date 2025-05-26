7f091d2e9978f4ceea5755157515e9220256e7bb
Luis <langelgirona@gmail.com>
2025-05-26 08:12:21 -0600

    Add Login and SignUp components to Principal layout

--- src/paginas/Principal.jsx
+++ src/paginas/Principal.jsx
@@ -2,12 +2,17 @@ import "../css/Principal.css"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
 import TrendingGames from "../components/TrendingGames"
+import React from "react"
+import SignUp from "../components/SignUp"
+import Login from "../components/Login"
 
 function App() {
   return (
     <div className="app">
       <div className="background-overlay">
         <Header />
+        <Login onLogin={user => console.log("Usuario logueado:", user)} />
+        <SignUp />
         <main>
           <Hero />
           <TrendingGames />
